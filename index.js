3ed7cd07a2bbbc7f2019d5633d7d4679a8986988
Elizabeth Dowling <dowlinge94@gmail.com>
2022-03-16 09:25:44 -0400

    commit tree b0640f40a5253c8cb901eae441778a7a2cf96e42

--- index.js
+++ index.js
@@ -1,11 +1,38 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+// let someValue = 199;
+function scuberGreetingForFeet(someValue) {
+  if(someValue <= 400){
+      return 'This one is on me!'
+  } else if (someValue > 2000 && someValue < 2500) {
+    return 'I will gladly take your thirty bucks.'
+  } else {
+    return 'No can do.'
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+// function ternaryCheckCity(cityName){
+//   // Write your code here!
+//   return (cityName === 'NYC' ? 'Ok, sounds good.' : 'No go.')
+// }
+
+//NOTES using arrow function in place of traditional function dec.
+const ternaryCheckCity = (cityName) => {
+  return (cityName === 'NYC' ? 'Ok, sounds good.' : 'No go.')
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tipLevel){
   // Write your code here!
+  switch (tipLevel) {
+    case 'generous':
+      return 'Thank you so much.'
+    break;
+    case 'not as generous':
+      return 'Thank you.'
+    break;
+    default: return 'Bye.'    
+    break;
+  }
 }
+
+//generous Thank you very much.
+//not so generous Thank you.
+//anything else Bye.
